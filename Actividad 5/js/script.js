94b8c1faa83c36eb5ae66c1d829b59850d7338da
Mariafé Roces Yactayo <mrocyac985@g.educaand.es>
2025-03-09 04:16:48 +0100

    Add files via upload

---
+++ Actividad 5/js/script.js
@@ -0,0 +1,10 @@
+
+function mostrarmensaje() {
+        let nombre = document.getElementById("nombre").value; 
+        let edad = parseInt(document.getElementById("edad").value, 10);
+        document.getElementById("mensaje").innerHTML = " Hola " + nombre + ", tienes " + edad + " años.";
+    }
+
+
+    
+  
