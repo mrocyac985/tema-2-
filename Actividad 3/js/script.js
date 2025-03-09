d841a01a95d9b1d653a7273c7529de915ec3eb35
Mariafé Roces Yactayo <mrocyac985@g.educaand.es>
2025-03-09 17:35:24 +0100

    Update script.js

--- Actividad 3/js/script.js
+++ Actividad 3/js/script.js
@@ -1,8 +1,5 @@
-
-function Pedirdatos() {
+function Pedirdatos(){
     let nombre = document.getElementById("nombre").value; 
     let edad = parseInt(document.getElementById("edad").value, 10);
     document.getElementById("mensaje").innerHTML = " Hola " + nombre + ", tienes " + edad + " años.";
 }
-
-
